b3f48d281c30df200ff3160539a81286a8c93cc5
Darshan Bagul <darshan.bagul@gmail.com>
2017-08-22 23:02:49 -0400

    Initial commit

---
+++ deepTesla.js
@@ -0,0 +1,13 @@
+{
+	"network" : [
+		{ "type" : "input", "out_sx" : 200, "out_sy" : 66, "out_depth" : 3 },
+		{ "type" : "conv", "sx" : 3, "filters" : 8, "stride" : 1, "pad" : 2, "activation" : "relu" },
+		{ "type" : "pool", "sx" : 2, "stride" : 2},
+		{ "type" : "conv", "sx" : 3, "filters" : 8, "stride" : 1, "pad" : 2, "activation" : "relu" },
+		{ "type" : "pool", "sx" : 2, "stride" : 2},
+		{ "type" : "conv", "sx" : 3, "filters" : 8, "stride" : 1, "pad" : 2, "activation" : "relu" },
+		{ "type" : "pool", "sx" : 3, "stride" : 3 },
+		{ "type" : "regression", "num_neurons" : 1 }
+	],
+	"trainer" : { "method" : "adadelta", "batch_size" : 4, "l2_decay" : 0.0001 }
+}
